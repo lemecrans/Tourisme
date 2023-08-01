01b8859efbaf022ef17cd9e77252ca74e868561e
Valisoa Ranaivoson <mickayarans@outlook.fr>
2023-08-01 16:58:13 +0300

    Update agence.js

--- src/models/agence.js
+++ src/models/agence.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    idAgence: {type: String, required: true,},
     designation: { type: String, required: true, },
     email: { type: String, required: true, },
     password: { type: String, required: true, },
@@ -11,7 +10,9 @@ const schema = new Schema({
     publication: [{
         description: { type: String, required: true, },
         photo: { type: [String], required: true, },
-    }],    
+    },
+    ],
+    Site: { type: String, required: true, },   
 }, { timestamps: true });
 
 module.exports = Agence = mongoose.model('Agence', schema)
